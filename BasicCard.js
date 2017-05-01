7da9c40085cb5c991fad2629fa683a7921456f26
Gerald Poyntz <Gerald Poyntz>
2017-05-01 13:06:05 -0400

    initial info

---
+++ BasicCard.js
@@ -0,0 +1,32 @@
+
+
+
+var BasicCard = function(front, back){
+    //front of card text
+    this.front = [],
+    //back of card text
+    this.back = [], 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//exporting BasicCard constructor
+module.exports = BasicCard;
---
+++ ClozeCard.js
@@ -0,0 +1,35 @@
+
+
+var ClozeCard = function(text, cloze){
+    //ONLY deleted portion of the text
+    this.cloze = []
+    //ONLY partial text
+    this.partial
+    //ONLY the full text
+    this.fullText
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//exporting ClozeCard constructor
+module.exports = ClozeCard;
